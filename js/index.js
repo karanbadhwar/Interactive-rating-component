f2850d2fbd74f9ab6e246fdb4f2cf14bb079a0d1
karanbadhwar <karanbadhwar07@gmail.com>
2022-06-26 08:52:02 -0400

    all the files
    All the files uploaded

---
+++ js/index.js
@@ -0,0 +1,43 @@
+let submit = document.getElementById('submit');
+let ul = document.getElementById('list');
+let container = document.getElementById('container');
+let body = document.getElementsByTagName('body')[0];
+let rating ='';
+
+function flipCard(e){
+    // console.log(e.target)
+    if(rating){
+        const div = ` <div id="rating">
+                        <img class='thankyou' src="./images/illustration-thank-you.svg"/>
+                        <p class='rating'>You Selected  ${rating} out of 5</p>
+                        <h2>Thank You</h2>
+                        <p class='note'>We appreciate you taking the time to give a rating. If you ever need more support,don’t hesitate to get in touch!</p>
+                    </div>
+        `;
+        container.innerHTML = div;
+        let warn = document.getElementById('warning');
+        warn.style.display='none';
+    }else{
+        const warning = `
+                    <h3 id='warning'>Please Select One Number for Rating
+        `;
+        body.insertAdjacentHTML('beforebegin', warning);
+        // 
+        // setInterval(()=>{
+        //     warn.style.display='none'
+        // },2000);
+        // setInterval(() =>{
+        //     warn.style.display='block'
+        // },3000);
+    }
+
+
+}
+
+
+ul.addEventListener('click', (e) =>{
+    // console.log(e.target.value);
+    rating+= e.target.value;
+});
+
+submit.addEventListener('click', flipCard);
